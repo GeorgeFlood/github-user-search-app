aa241d3beb0bb84a2a585c461b00a15590e1cc01
GeorgeFlood <mrgeorgeflood@gmail.com>
2022-03-26 08:03:52 +0000

    finished
    The code is messy, but it works as expected.
    
    I didn't want to spend a long time with this project so it'll only work with desktop/laptop view.

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const website = document.querySelector('#website');
 const twitter = document.querySelector('#twitter');
 const company = document.querySelector('#company');
 const searchBar = document.querySelector('#search');
+const lightOrDark = document.querySelector('#lightOrDark');
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+
+
+
 
 const request = new XMLHttpRequest();
 request.open('GET', 'https://api.github.com/users/octocat');
@@ -18,9 +24,9 @@ request.send();
 const userSearch = () => {
     searchBar.addEventListener('change', (e) => {
         let userName = e.target.value;
-        request.open('GET', `https://api.github.com/users/${userName}`)
+        request.open('GET', `https://api.github.com/users/${userName}`);
         request.send();
-        console.log(request)
+        console.log(request);
     });
 };
 
@@ -32,15 +38,77 @@ request.addEventListener('readystatechange', (e) => {
         console.log(data);
 
         profilePicture.src = `${data.avatar_url}`;
-        profileName.textContent = `${data.login}`
-        profileDate.textContent = `Joined ${data.created_at}`.substring(0, 17);;
-        data.bio === null ? bio.textContent = 'No Bio available' : bio.textContent = `${data.bio}`;
-        repos.textContent = `${data.public_repos}`
+        profileName.textContent = `${data.login}`;
+        profileDate.textContent = `Joined: ${data.created_at}`.substring(0, 17);
+        data.bio === null ?
+            (bio.textContent = 'No Bio available') :
+            (bio.textContent = `${data.bio}`);
+        repos.textContent = `${data.public_repos}`;
         followers.textContent = `${data.followers}`;
-        following.textContent = `${data.following}`
-        data.location === null ? location1.textContent = `Location: Anonymous` : location1.textContent = `Location: ${data.location}`;
-        data.blog === '' ? website.textContent = 'No website available' : website.textContent = `Website: ${data.blog}`
-        data.twitter_username === null ? twitter.textContent = 'No Twitter' : twitter.textContent = `Twitter: ${data.twitter_username}`;
-        data.company === null ? company.textContent = 'Not available' : company.textContent = `Company: ${data.company}`;
+        following.textContent = `${data.following}`;
+        data.location === null ?
+            (location1.textContent = `Location: Anonymous`) :
+            (location1.textContent = `Location: ${data.location}`);
+        data.blog === '' ?
+            (website.textContent = 'No website available') :
+            (website.textContent = `Website: ${data.blog}`);
+        data.twitter_username === null ?
+            (twitter.textContent = 'No Twitter') :
+            (twitter.textContent = `Twitter: ${data.twitter_username}`);
+        data.company === null ?
+            (company.textContent = 'Not available') :
+            (company.textContent = `Company: ${data.company}`);
     }
+
+});
+
+
+sun.addEventListener('click', (e) => {
+    const searchSection = document.querySelector('.searchSection');
+    const input = document.querySelector('.input');
+    const main = document.querySelector('.main');
+    const title = document.querySelector('.title');
+    const container = document.querySelector('.stats');
+    searchSection.className = 'searchDark';
+    input.className = 'darkInput';
+    main.className = 'darkMain';
+    title.className = 'darkTitle';
+    container.className = 'statsDark';
+    document.documentElement.className = 'bodyDark';
+    // document.querySelector('.title').style.color = 'white';
+    // document.querySelector('.searchSection').style.backgroundColor = '#1F2B48';
+    // document.querySelector('#search').style.backgroundColor = '#1F2B48';
+    // document.querySelector('#main').style.backgroundColor = '#1F2B48'
+    // document.querySelector('#main').style.boxShadow = 'none';
+    // document.querySelector('.searchSection').style.boxShadow = 'none';
+    // document.querySelector('#main').style.color = 'white';
+    sun.style.display = 'none';
+    moon.style.display = 'inline-block';
+    lightOrDark.innerHTML = 'Dark';
+    lightOrDark.style.color = 'white';
 })
+
+
+moon.addEventListener('click', (e) => {
+    const night = document.querySelector('.lightOrDark');
+    const searchSection = document.querySelector('.searchDark');
+    const input = document.querySelector('.darkInput');
+    const main = document.querySelector('.darkMain');
+    const title = document.querySelector('.darkTitle');
+    const container = document.querySelector('.statsDark');
+
+    searchSection.className = 'searchSection';
+    input.className = 'input';
+    main.className = 'main';
+    title.className = 'title';
+    container.className = 'stats';
+    document.documentElement.className = 'body';
+    sun.style.display = 'inline-block';
+    moon.style.display = 'none';
+    lightOrDark.innerHTML = 'Light';
+    lightOrDark.style.color = 'black';
+});
+
+
+
+// there must be a toggle option but this works for now!!
